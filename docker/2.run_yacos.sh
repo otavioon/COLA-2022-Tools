e490fbba412499c2b01a0e79d537cc5a3762f807
Otavio Napoli <otavio.napoli@gmail.com>
2022-06-15 12:34:43 +0100

    Build Dockerfile

---
+++ docker/2.run_yacos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script utilizado para execução do playbook configure.yaml nas máquinas do inventorio.
+# Este script não deve ser alterado
+
+source ./vars.sh
+
+$CONTAINER_CMD run --interactive --tty --rm \
+  --env "HOME=$VOLUME_DIR" \
+  --env "SHELL=/bin/bash" \
+  --workdir $VOLUME_DIR \
+  --volume $VOLUME_DIR:$VOLUME_DIR \
+  $CONTAINER_IMAGE bash
---
+++ docker/vars.sh
@@ -0,0 +1,4 @@
+CONTAINER_CMD="docker"                      # Aplicação de container a ser utilizada
+CONTAINER_IMAGE="cola-2022"                 # Nome da imagem
+VOLUME_DIR="$(realpath $(pwd))/.."
+WORK_DIR="$(realpath $(pwd))/.."            # Diretório
