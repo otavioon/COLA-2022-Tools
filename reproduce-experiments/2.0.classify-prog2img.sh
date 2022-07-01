8c48bed08f32bcf006eb64166018458134102c19
Nilton <niltonlqjr@gmail.com>
2022-06-30 21:35:27 -0300

    update scripts (lbpif/eq) to run loop with 2 classes and add script to run prog2image classification

--- reproduce-experiments/2.0.classify-prog2img.sh
+++ reproduce-experiments/2.0.classify-prog2img.sh
@@ -15,18 +15,18 @@ done
 
 DATASET='loop/anghabench'
 
-mkdir ${RESULT_DIR}/${DATASET}
+mkdir ${RESULT_DIR}/${DATASET}_2C
 mkdir ${RESULT_DIR}/${DATASET}_2C/${REPRESENTATION}
 
 python3 $ALG_DIR/classify.py --dataset_directory=${REPRESENTATION_DIR}/${DATASET}/${REPRESENTATION} \
-                --dataset_description=${DATASET_DIR}/${DATASET}/dataset_description_2C_small.yaml \
-                --results_directory=${RESULT_DIR}/${DATASET}_2C/${REPRESENTATION} --rounds=1 --epochs=10
+                --dataset_description=${DATASET_DIR}/${DATASET}/dataset_description_2C.yaml \
+                --results_directory=${RESULT_DIR}/${DATASET}_2C/${REPRESENTATION}
 
 for DATASET in ${DATASETS[@]}
 do
     mkdir ${RESULT_DIR}/${DATASET}
     mkdir ${RESULT_DIR}/${DATASET}/${REPRESENTATION}
     python3 $ALG_DIR/classify.py --dataset_directory=${REPRESENTATION_DIR}/${DATASET}/${REPRESENTATION} \
-            --dataset_description=${DATASET_DIR}/${DATASET}/dataset_description_small.yaml \
-            --results_directory=${RESULT_DIR}/${DATASET}/${REPRESENTATION} --rounds=1 --epochs=10
+            --dataset_description=${DATASET_DIR}/${DATASET}/dataset_description.yaml \
+            --results_directory=${RESULT_DIR}/${DATASET}/${REPRESENTATION}
 done
--- reproduce-experiments/2.2.classify-lbpeq.sh
+++ reproduce-experiments/2.2.classify-lbpeq.sh
@@ -15,12 +15,12 @@ done
 
 DATASET='loop/anghabench'
 
-mkdir ${RESULT_DIR}/${DATASET}
+mkdir ${RESULT_DIR}/${DATASET}_2C
 mkdir ${RESULT_DIR}/${DATASET}_2C/${REPRESENTATION}
 
 python3 $ALG_DIR/classify.py --dataset_directory=${REPRESENTATION_DIR}/${DATASET}/${REPRESENTATION} \
                 --dataset_description=${DATASET_DIR}/${DATASET}/dataset_description_2C.yaml \
-                --results_directory=${RESULT_DIR}/${DATASET}_2C/${REPRESENTATION} --rounds=1
+                --results_directory=${RESULT_DIR}/${DATASET}_2C/${REPRESENTATION}
 
 for DATASET in ${DATASETS[@]}
 do
@@ -28,5 +28,5 @@ do
     mkdir ${RESULT_DIR}/${DATASET}/${REPRESENTATION}
     python3 $ALG_DIR/classify.py --dataset_directory=${REPRESENTATION_DIR}/${DATASET}/${REPRESENTATION} \
             --dataset_description=${DATASET_DIR}/${DATASET}/dataset_description.yaml \
-            --results_directory=${RESULT_DIR}/${DATASET}/${REPRESENTATION} --rounds=1
+            --results_directory=${RESULT_DIR}/${DATASET}/${REPRESENTATION}
 done
