73f000aa5dc107251bfde804663c9e1c22ea0c81
Otavio Napoli <otavio.napoli@gmail.com>
2022-07-20 23:57:28 +0000

    Added some figures

--- generate-charts.sh
+++ generate-charts.sh
@@ -1,9 +1,11 @@
 #python3 scripts/plot_accuracy-v1.py -r results-v1 -d POJ-5_TVT -f figures-v1/POJ-5_acc.pdf
 #python3 scripts/plot_accuracy-v1.py -r results-v1 -d AnghaLoops_TVT -f figures-v1/AnghaLoops_acc.pdf
 
-python3 scripts/plot_accuracy-v1.py -r results/algo -d poj -f figures-v2/algo/figure10_poj.pdf
-python3 scripts/plot_accuracy-v1.py -r results/algo -d codenet -f figures-v2/algo/figure11_codenet.pdf
-python3 scripts/plot_accuracy-v1.py -r results/loop -d angabench -f figures-v2/loop/figure12_anghabench.pdf
-python3 scripts/plot_accuracy-v1.py -r results/optz -d poj -f figures-v2/optz/figure14_poj.pdf
-python3 scripts/plot_accuracy-v1.py -r results/optz -d codenet -f figures-v2/optz/figure15_codenet.pdf
-python3 scripts/plot_accuracy-v1.py -r results/optz -d angabench -f figures-v2/optz/figure16_angabench.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/algo -d poj -f figures/algo/figure10_poj.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/algo -d codenet -f figures/algo/figure11_codenet.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/loop -d anghabench -f figures/loop/figure12_anghabench.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/loop -d anghabench_2C -f figures/loop/figure13_anghabench_2C.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/optz -d poj -f figures/optz/figure14_poj.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/optz -d codenet -f figures/optz/figure15_codenet.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/optz -d anghabench -f figures/optz/figure16_angabench.pdf
+python3 scripts/plot_accuracy-v1.py --rename -r results/optz -d poj_s -f figures/optz/figure17_poj_s.pdf
--- scripts/plot_accuracy-v1.py
+++ scripts/plot_accuracy-v1.py
@@ -23,14 +23,18 @@ def collect_results(results_directory: Path, dataset_dir: str):
     results = defaultdict(list)
     for representation_dir in search_dir.glob("*"):
         print(f"Looking at {representation_dir}....")
-        for filepath in representation_dir.rglob("test_metrics.yaml"):
-            accuracy = read_accuracy(filepath)
-            representation = representation_dir.stem
-            results[representation].append(accuracy)
+        for filepath in representation_dir.rglob("test_metrics*.yaml"):
+            try:
+                accuracy = read_accuracy(filepath)
+                representation = representation_dir.stem
+                results[representation].append(accuracy)
+            except Exception as e:
+                print(f"Error for {filepath}: {e.__class__.__name__}: {e}")
+                continue
 
     return dict(results)
 
-def plot_v_box_plot(results, output_filename: Path = None):
+def plot_v_box_plot(results, output_filename: Path = None, rename_map: dict = None):
     fig, ax = plt.subplots(figsize =(8, 4))
 
     # Collect data
@@ -38,8 +42,13 @@ def plot_v_box_plot(results, output_filename: Path = None):
     sorted_results.sort()
     labels = []
     datasets = []
-    for resultlabel, res in sorted_results:
-        labels.append(resultlabel) # Cut three first digits
+    for result_label, res in sorted_results:
+        if result_label == "cdfg_plus_ir2vec":
+            continue
+        if rename_map:
+            labels.append(rename_map[result_label]) # Cut three first digits
+        else:
+            labels.append(result_label)
         datasets.append(res)
 
     # Vertical Box Plor
@@ -50,7 +59,7 @@ def plot_v_box_plot(results, output_filename: Path = None):
     ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
     #ax.set_title('CHART TITLE')
     ax.grid()
-    ax.set_xticklabels(labels,fontname="Times")
+    ax.set_xticklabels(labels, fontname="Times")
     fig.autofmt_xdate(rotation=45)
     if output_filename is not None:
         output_filename.parent.mkdir(parents=True, exist_ok=True)
@@ -64,6 +73,7 @@ if __name__ == '__main__':
     parser.add_argument("-r", "--results_directory", help="Results directory")
     parser.add_argument("-d", "--dataset", help="Dataset subdirectory name")
     parser.add_argument("-f", "--filename", help="Output filename")
+    parser.add_argument("--rename", action="store_true", help="Perform renaming of labels")
     args = parser.parse_args()
 
     import matplotlib
@@ -73,4 +83,34 @@ if __name__ == '__main__':
     #rc('text', usetex=True)
 
     results = collect_results(Path(args.results_directory), args.dataset)
-    plot_v_box_plot(results, Path(args.filename))
+    renames = {
+        "cdfg_compact_histogram_IR_O0": "cdfg_c : histogram",
+        "cdfg_compact_inst2vec_IR_O0": "cdfg_c : inst2vec",
+        "cdfg_compact_ir2vec_IR_O0": "cdfg_c : ir2vec",
+        "cdfg_histogram_IR_O0": "cdfg : histogram",
+        "cdfg_inst2vec_IR_O0": "cdfg : inst2vec",
+        "cdfg_ir2vec_IR_O0": "cdfg : ir2vec",
+        "cdfg_plus_histogram_IR_O0": "cdfg+ : histogram",
+        "cdfg_plus_inst2vec_IR_O0": "cdfg+ : inst2vec",
+        "cdfg_plus_ir2vec_IR_O0": "cdfg+ : ir2vec",
+        "cfg_compact_histogram_IR_O0": "cfg_c : histogram",
+        "cfg_compact_inst2vec_IR_O0": "cfg_c : inst2vec",
+        "cfg_compact_ir2vec_IR_O0": "cfg_c : ir2vec",
+        "cfg_histogram_IR_O0": "cfg : histogram",
+        "cfg_inst2vec_IR_O0": "cfg : inst2vec",
+        "cfg_ir2vec_IR_O0": "cfg : ir2vec",
+        "histogram_IR_O0": "histogram",
+        "ir2vec_IR_O0": "ir2vec",
+        "milepost_IR_O0": "milepost",
+        "programl_histogram_IR_O0": "programl : histogram",
+        "programl_inst2vec_IR_O0": "programl : inst2vec",
+        "programl_ir2vec_IR_O0": "programl : ir2vec",
+        "lbpeq": "lbpeq",
+        "lbpif": "lbpif",
+        "prog2img": "prog2img"
+    }
+
+    if not args.rename:
+        renames = None
+
+    plot_v_box_plot(results, Path(args.filename), rename_map=renames)
